7d918c80bfa520463045da1aa6de77228fb35038
Dan Chaffelson <dan@tinybird.co>
2023-08-08 13:10:31 +0100

    further refactoring

--- modules/db_functions.py
+++ modules/db_functions.py
@@ -1,7 +1,6 @@
 # db_functions.py
 
 import psycopg2
-from psycopg2 import sql
 import mysql.connector
 from .utils import Config
 
@@ -25,37 +24,6 @@ def mysql_connect_db():
     print(f"Connected to the MySQL database. id: {conn.connection_id}")
     return conn
 
-def mysql_table_create(conn, table_name):
-    try:
-        print(f"Creating the {table_name} table if not exists...")
-        cur = conn.cursor()
-
-        # Create Users table if not exists
-        # Note that this schema matches the Tinybird handling for this table, so changing the schema without updating the Tinybird files will break the overall pipeline.
-        cur.execute(f'''
-        CREATE TABLE IF NOT EXISTS {table_name} (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(100),
-            email VARCHAR(100),
-            address VARCHAR(100),
-            phone_number VARCHAR(50),
-            email_verified BOOLEAN DEFAULT FALSE,
-            onboarded BOOLEAN DEFAULT FALSE,
-            deleted BOOLEAN DEFAULT FALSE,
-            lang CHAR(2),
-            created_at TIMESTAMP,
-            updated_at TIMESTAMP
-        )
-        ''')
-        cur.fetchall()  # Ensure all results are read
-        conn.commit()
-
-        cur.close()
-        print("Users table created successfully.")
-    except Exception as e:
-        print(f"Error creating table: {e}")
-        conn.rollback()
-
 def pg_connect_db():
     print("Connecting to the Postgres database...")
     # Connect to PostgreSQL
@@ -68,36 +36,49 @@ def pg_connect_db():
     print("Connected to the Postgres database.")
     return conn
 
-def pg_table_create(conn, table_name):
+def test_db_connection(db_type):
+    try:
+        if db_type == 'PG':
+            conn = pg_connect_db()
+            query = """
+                SELECT COUNT(*) 
+                FROM information_schema.tables 
+                WHERE table_schema = 'public'
+            """
+        elif db_type == 'MYSQL':
+            conn = mysql_connect_db()
+            query = """
+                SELECT COUNT(*)
+                FROM information_schema.tables 
+                WHERE table_schema = DATABASE()
+            """
+        else:
+            print(f'Invalid database type: {db_type}')
+        cur = conn.cursor()
+        cur.execute(query)
+        num_tables = cur.fetchone()[0]
+        cur.close()
+        print(f'Number of tables in {db_type} database: {num_tables}')
+    except Exception as e:
+        print(f'Error connecting to {db_type}: {e}')
+
+def table_create(conn, table_name, query):
     try:
         print(f"Creating the {table_name} table if not exists...")
         cur = conn.cursor()
+        cur.execute(query)
+        
+        # Check if the connection is a MySQL connection
+        if isinstance(conn, mysql.connector.MySQLConnection):
+            cur.fetchall()  # Ensure all results are read for MySQL
 
-        # Create Users table if not exists
-        # Note that this schema matches the Tinybird handling for this table, so changing the schema without updating the Tinybird files will break the overall pipeline.
-        create_table_query = sql.SQL('''
-        CREATE TABLE IF NOT EXISTS {} (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(100),
-            email VARCHAR(100),
-            address VARCHAR(100),
-            phone_number VARCHAR(50),
-            email_verified BOOLEAN DEFAULT FALSE,
-            onboarded BOOLEAN DEFAULT FALSE,
-            deleted BOOLEAN DEFAULT FALSE,
-            lang CHAR(2),
-            created_at TIMESTAMP,
-            updated_at TIMESTAMP
-        )
-        ''').format(sql.Identifier(table_name))
-        cur.execute(create_table_query)
         conn.commit()
-
         cur.close()
-        print("Users table created successfully.")
+        print(f"{table_name} table created successfully.")
     except Exception as e:
         print(f"Error creating table: {e}")
         conn.rollback()
+
 
 def table_fetch(conn, table_name):
     print("Fetching the current list of users...")
@@ -124,7 +105,7 @@ def table_drop(conn, table_name):
     cur.close()
     print(f"{table_name} table dropped if it existed.")
 
-def get_column_names(conn, table_name):
+def table_column_names(conn, table_name):
     cur = conn.cursor()
     cur.execute(f'SELECT * FROM {table_name} LIMIT 0')
     column_names = [desc[0] for desc in cur.description]
